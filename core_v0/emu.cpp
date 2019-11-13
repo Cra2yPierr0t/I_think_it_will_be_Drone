140672f8b76034bfc96e2ac97003ec3d9d9ce2f2
pirrot <uchiyama.kazuhide@gmail.com>
2019-11-13 23:11:18 +0900

    wakaran

--- core_v0/emu.cpp
+++ core_v0/emu.cpp
@@ -3,8 +3,8 @@
 #include<bitset>
 
 uint32_t reg[32];
-uint32_t pc;
-std::vector<uint32_t> memory = {0xffffffff, 0xeeeeeeee};
+uint32_t pc = 0;
+std::vector<uint32_t> memory = {0x00000113, 0x00000193, 0x0640093, 0x00110113, 0x002181b3, 0xfe111ce3, 0x0000026f};
 
 typedef enum {
     R_type,
@@ -30,16 +30,17 @@ struct Operation {
 };
 
 //現在着目してる命令
-Operation *operation;
+Operation *operation = new Operation;
 
 void fetch(Operation *operation){
-    operation->bin = memeory[pc];
+    operation->bin = memory[pc];
 }
 
 void decode(Operation *operation){
     int i;
-    for(i = 0;i < 7;i++)
-        operation->opcode |= operation->bin[i];
+    for(i = 0;i < 7;i++){
+        operation->opcode |= (operation->bin[i] << i);
+    }
 
     switch(operation->opcode){
         case 0x33:
@@ -65,7 +66,11 @@ void decode(Operation *operation){
             for(i = 0;i < 3;i++)
                 operation->funct3 |= operation->bin[i + 12] << i;
             for(i = 0;i < 12;i++)
-                operation->imm |= operation->bin[i + 25] << i;
+                operation->imm |= (operation->bin[i + 20] << i);
+            printf("rs1     = %d\n", operation->rs1);
+            printf("rd      = %d\n", operation->rd);
+            printf("funct3  = %d\n", operation->funct3);
+            printf("imm     = %d\n", operation->imm);
             break;
         case 0x23:
             operation->format = S_type;
@@ -110,14 +115,87 @@ void decode(Operation *operation){
 }
 
 void execute(Operation *operation){
-    
+    switch(operation->opcode){
+        case 0x33:
+            switch(operation->funct3){
+                case 0x00:
+                    reg[operation->rd] = reg[operation->rs1] + reg[operation->rs2];
+                    pc++;
+                    break;
+                case 0x07:
+                    reg[operation->rd] = reg[operation->rs1] & reg[operation->rs2];
+                    pc++;
+                    break;
+                case 0x06:
+                    reg[operation->rd] = reg[operation->rs1] | reg[operation->rs2];
+                    pc++;
+                    break;
+                case 0x04:
+                    reg[operation->rd] = reg[operation->rs1] ^ reg[operation->rs2];
+                    pc++;
+                    break;
+            }
+        break;
+        case 0x03:
+            switch(operation->funct3){
+                case 0x02:
+                    reg[operation->rd] = memory[operation->rs1 + operation->imm];
+                    pc++;
+                    break;
+            }
+        break;
+        case 0x13:
+            switch(operation->funct3){
+                case 0x00:
+                    reg[operation->rd] = operation->imm + reg[operation->rs1];
+                    pc++;
+                    break;
+                case 0x07:
+                    reg[operation->rd] = operation->imm & reg[operation->rs1];
+                    pc++;
+                    break;
+                case 0x06:
+                    reg[operation->rd] = operation->imm | reg[operation->rs1];
+                    pc++;
+                    break;
+                case 0x04:
+                    reg[operation->rd] = operation->imm ^ reg[operation->rs1];
+                    pc++;
+                    break;
+            }
+        break;
+        case 0x23:
+            switch(operation->funct3){
+                case 0x02:
+                    memory[reg[operation->rs1] + operation->imm] = reg[operation->rs2];
+                    pc++;
+                    break;
+            }
+        break;
+        case 0x6f:
+            reg[operation->rd] = ++pc;
+            pc = operation->imm;
+        break;
+        case 0x63:
+            if(reg[operation->rs1] == reg[operation->rs2]){
+                pc += operation->imm;
+            }else{
+                pc++;
+            }
+        break;
+    }
 }
 
 int main(int argc, char **argv){
-    while(1){
+    int cnt = 0;
+    while(cnt != 20){
         fetch(operation);
         decode(operation);
         execute(operation);
-        writeback(operation);
+        cnt++;
     }
+    for(int i = 0;i < 32; i++){
+        printf("reg[%d] = %d\n", i, reg[i]);
+    }
+    return 0;
 }
