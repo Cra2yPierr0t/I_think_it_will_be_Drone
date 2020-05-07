7375fe7c5a32722668ebec552b5f12425646baf7
cra2ypierr0t <uchiyama.kazuhide@gmail.com>
2020-05-07 12:28:47 +0900

    fix

--- accelerometer_test_v3/obj_dir/Vacc_driver__ALLcls.d
+++
@@ -1,3 +0,0 @@
-Vacc_driver__ALLcls.o: Vacc_driver__ALLcls.cpp Vacc_driver.cpp \
- Vacc_driver.h /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h Vacc_driver__Syms.h
--- accelerometer_test_v3/obj_dir/acc_driver_test.d
+++
@@ -1,5 +0,0 @@
-acc_driver_test.o: ../acc_driver_test.cpp \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h Vacc_driver.h
