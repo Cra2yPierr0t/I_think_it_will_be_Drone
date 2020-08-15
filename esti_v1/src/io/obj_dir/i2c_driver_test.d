829b517ca372446d4109600ff00c543beecb68ca
cra2ypierr0t <uchiyama.kazuhide@gmail.com>
2020-08-15 20:19:12 +0900

    [fix] .gitignore

--- esti_v1/src/io/obj_dir/i2c_driver_test.d
+++
@@ -1,6 +0,0 @@
-i2c_driver_test.o: ../i2c_driver_test.cpp \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilated_trace.h Vi2c_driver.h
